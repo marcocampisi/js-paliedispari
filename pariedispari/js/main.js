9ffcbeeca46bb729b30c520e1cb837ad5261ca39
marcocampisi <marco.campisi@outlook.it>
2023-06-20 16:39:00 +0200

    Add complete logic

--- pariedispari/js/main.js
+++ pariedispari/js/main.js
@@ -16,10 +16,28 @@ const userInput = prompt('Scegli pari o dispari:');
 const userNumber = parseInt(prompt('Inserisci un numero da 1 a 5: '));
 
 if (userInput !== 'pari' && userInput !== 'dispari') { //Validazione input
-    alert('Scelta non valida, riprova.')
-} else if (userNumber < 1 || userNUmber > 5) {
-    alert('Numero non valido, riprova.')
+    alert('Scelta non valida, riprova.');
+} else if (userNumber < 1 || userNumber > 5) {
+    alert('Numero non valido, riprova.');
 } else {
     const computerNumber = getRndInteger(1, 5);
     alert(`Il numero del computer è ${computerNumber}`);
+
+    const sum = userNumber + computerNumber;
+    alert(`La somma dei due numeri è: ${somma}`);
+
+    let result;
+    if (isEven(sum)) {
+        result = 'pari';
+        alert('La somma è pari.');
+    } else {
+        result = 'dispari';
+        alert('La somma è dispari.');
+    }
+
+    if (result === userInput) {
+        alert('Hai vinto!');
+    } else {
+        alert('Hai perso!');
+    }
 }
