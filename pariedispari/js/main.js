32cda6db6a187d81e62dc8fbe9416095c5c5c62b
marcocampisi <marco.campisi@outlook.it>
2023-06-20 16:29:47 +0200

    Add validate input

--- pariedispari/js/main.js
+++ pariedispari/js/main.js
@@ -9,3 +9,17 @@ function getRndInteger(min, max) {
 function isEven(number) {
     return numero % 2 === 0;
 }
+
+//Logica base del programma
+
+const userInput = prompt('Scegli pari o dispari:');
+const userNumber = parseInt(prompt('Inserisci un numero da 1 a 5: '));
+
+if (userInput !== 'pari' && userInput !== 'dispari') { //Validazione input
+    alert('Scelta non valida, riprova.')
+} else if (userNumber < 1 || userNUmber > 5) {
+    alert('Numero non valido, riprova.')
+} else {
+    const computerNumber = getRndInteger(1, 5);
+    alert(`Il numero del computer è ${computerNumber}`);
+}
