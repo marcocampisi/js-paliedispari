e19197c2c7a7805de71f374c9baa731426754d94
marcocampisi <marco.campisi@outlook.it>
2023-06-20 16:12:46 +0200

    Add comments

--- palindroma/js/main.js
+++ palindroma/js/main.js
@@ -1,11 +1,13 @@
 function isPalindrome(word) {
+    //Manipolo la stringa input per generare la parola al contario senza spazi e minuscola
     const reverseWord = word.toLowerCase().split('').reverse('').join('');
+    //Restituisco True se la stringa è palindroma, altrimenti False
     return word.toLowerCase() === reverseWord;
 }
 
-const inputWord = prompt('Inserisci una parola:');
+const inputWord = prompt('Inserisci una parola:'); //Input dell'utente
 
-if (isPalindrome(inputWord)) {
+if (isPalindrome(inputWord)) { //Restituisco il risultato
     alert('La parola inserita è palindroma')
 }
 else {
