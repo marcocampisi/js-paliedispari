9fb5ab52fa356004483336ac90164e6bb81e2afa
marcocampisi <marco.campisi@outlook.it>
2023-06-20 16:17:57 +0200

    Add isEven() function

--- pariedispari/js/main.js
+++ pariedispari/js/main.js
@@ -2,4 +2,10 @@
 
 function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min + 1) ) + min;
-  }
+}
+
+//Funzione per verificare se il numero è pari
+
+function isEven(number) {
+    return numero % 2 === 0;
+}
